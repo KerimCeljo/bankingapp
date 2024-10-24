7a632ee88096472573546cbe289fc93f49d5751a
KerimCeljo <kerim.celjo@stu.ibu.edu.ba>
2024-10-24 13:44:39 +0200

    Right side bar implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 
 const Home = () => {
-  const loggedIn = { firstName: 'Kerim' };
+  const loggedIn = { firstName: 'Kerim', lastName: 'JSM', email: 'kerim@gmail.com' };
   return (
     <section className="home">
         <div className="home-content">
@@ -21,7 +22,16 @@ const Home = () => {
             totalCurrentBalance={1250.35}
             />
           </header>
+
+          RECENT TRANSACTIONS
         </div>
+
+        <RightSidebar
+          user={loggedIn}
+          transactions={[]}
+          banks={[]}
+
+        />
     </section>
   )
 }
